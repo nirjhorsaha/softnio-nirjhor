3c36f22c8f4f976c01b41c077693afc3ca21a8b5
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-12-20 18:40:54 +0600

    set product in cart

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,9 @@ function App() {
     const [selectedColor, setSelectedColor] = useState("Indigo");
     const [selectedSize, setSelectedSize] = useState("S");
     const [quantity, setQuantity] = useState(0);
-    const [showModal, setShowModal] = useState(false); // State to control modal visibility
+    const [showModal, setShowModal] = useState(false);
+    const [cart, setCart] = useState([]); 
+
 
     const handleDecrease = () => {
         if (quantity > 0) setQuantity(quantity - 1);
@@ -25,11 +27,11 @@ function App() {
     };
 
     const handleCheckout = () => {
-        setShowModal(true); // Show modal when checkout button is clicked
+        setShowModal(true);
     };
 
     const handleCloseModal = () => {
-        setShowModal(false); // Close modal
+        setShowModal(false);
     };
 
     // Function to set image based on the selected color
@@ -63,19 +65,35 @@ function App() {
         }
     }
 
+    // function to handle add to cart
+    const handleAddToCart = () => {
+        const productDetails = {
+            image: getImage(),
+            name: "Classy Modern Smart Watch", 
+            size: selectedSize,
+            quantity: quantity, // Add the quantity to the product details
+            price: selectedSize === "S" ? 69 : selectedSize === "M" ? 79 : selectedSize === "L" ? 89 : 99, 
+        };
+        setCart((prevCart) => [...prevCart, productDetails]);
+
+        console.log("Added to cart:", cart);
+    };
+
+
     return (
         <div className="mx-auto">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[60px] w-[1320px] h-[720px] justify-center items-center">
+            <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-[60px] w-full lg:w-[1320px] h-auto lg:h-[720px] justify-center items-center ">
                 <img
                     src={getImage()}
-                    alt="Yellow Travel Bag image"
-                    className="h-[720px] w-[630px] mx-auto object-cover rounded-[4px]"
+                    alt="image"
+                    className="lg:h-[720px] w-full lg:w-[630px] mx-auto object-cover rounded-[4px]"
+
                 />
-                <div className=" w-[630px] h-[539px] text-left mx-auto ">
-                    <h2 className="h-[68px] font-[700] text-[44px] leading-[44px] text-[#364A63] tracking-[-1.2px] py-3">
+                <div className="w-full lg:w-[630px] h-auto lg:h-[539px] text-left mx-auto ">
+                    <h2 className="h-[68px] text-[32px] md:text-[38px] lg:text-[44px] font-[700] leading-[44px] tracking-[-1.2px] text-[#364A63] py-3">
                         Classy Modern Smart Watch
                     </h2>
-                    <div className="flex items-center gap-2 h-[28px]">
+                    <div className="flex items-center gap-2 h-[28px] mt-6 md:mt-0">
                         <div className="flex items-center gap-1">
                             <FaStar color="#FFD200" />
                             <FaStar color="#FFD200" />
@@ -94,7 +112,7 @@ function App() {
                             I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born, and I will give you a complete account of the system, and expound the actual teaching.
                         </p>
                     </div>
-                    <div className="flex gap-[43px] pt-5 pr-1  h-[67px]">
+                    <div className="flex gap-[43px] pt-16 md:pt-5 pr-1 h-[67px]">
                         <div>
                             <p className="font-roboto font-[400] text-[14px] leading-[23.1px] text-[#8091A7]">Type</p>
                             <p className="font-roboto font-[700] text-[16px] leading-[23px] text-[#364A63]">Watch</p>
@@ -104,7 +122,7 @@ function App() {
                             <p className="font-roboto font-[700] text-[16px] leading-[23px] text-[#364A63]">Forerunner 290XT</p>
                         </div>
                     </div>
-                    <div className=" h-[74px]">
+                    <div className="h-[74px] mt-14 md:mt-0">
                         <p className="font-[700] text-[18px] leading-5 text-[#364A63] mt-[20px] mb-[10px]">
                             Brand Color
                         </p>
@@ -194,7 +212,9 @@ function App() {
                                         <FiPlus />
                                     </button>
                                 </div>
-                                <button className="bg-[#6576FF]  px-[18px] py-[8px] rounded-[3px] flex items-center justify-center">
+                                <button onClick={handleAddToCart}
+
+                                    className="bg-[#6576FF]  px-[18px] py-[8px] rounded-[3px] flex items-center justify-center">
                                     <p className=" font-[700] text-[13px] leading-5 tracking-[0.26px] text-[#FFFFFF]">
                                         Add to Cart
                                     </p>
@@ -207,7 +227,7 @@ function App() {
                     </div>
                 </div>
             </div>
-            <div className="pt-[112px]">
+            <div className="pt-36 lg:pt-[112px] mx-auto w-full lg:w-[1320px]">
                 <button onClick={handleCheckout}
                     className="w-[139px] h-[42px] mx-auto text-center rounded-[20px] bg-[#FFBB5A] px-[24px] py-[8px] text-base shadow-xl ">
                     <p className="flex items-center justify-center gap-[10px] ">
@@ -219,21 +239,20 @@ function App() {
             </div>
             {showModal && (
                 <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                    <div className="bg-white p-[44px] rounded-[20px] w-[651px] h-[480px] space-y-4">
+                    {/* <div className="bg-white p-[44px] rounded-[20px] w-[651px] h-[480px] space-y-4 overflow-y-auto"> */}
+                    <div className="bg-white p-6 md:p-[44px] rounded-[20px] w-[90%] lg:w-[651px] h-auto md:h-[480px] overflow-y-auto space-y-4">
                         <h2 className="text-[#364A63] text-left font-bold text-[22px] leading-[24px]">Your Cart</h2>
-                        <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4 border-b border-[#DBDFEA] h-[36px] pt-1 pr-1 pb-2">
+                        <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4 md:border-b  border-[#DBDFEA] h-[36px] pt-1 pr-1 pb-2">
                             <div className="font-normal text-[14px] leading-[23.1px] text-[#8091A7] text-left">Item</div>
                             <div className="font-normal text-[14px] leading-[23.1px] text-[#8091A7]">Color</div>
                             <div className="font-normal text-[14px] leading-[23.1px] text-[#8091A7]">Size</div>
                             <div className="font-normal text-[14px] leading-[23.1px] text-[#8091A7]">Qnt</div>
                             <div className="font-normal text-[14px] leading-[23.1px] text-[#8091A7] text-right">Price</div>
                         </div>
-
-
                         {/* Cart item rows */}
-                        <div className="space-y-4 ">
+                        <div className="space-y-4">
                             {/* First Item */}
-                            <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4 border-b border-[#DBDFEA] pb-4 pr-1 ">
+                            <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4 md:border-b border-[#DBDFEA] pb-4 pr-1 ">
                                 <div className="flex items-center gap-2">
                                     <img src={Indigo} alt="Item" className="w-[36px] h-[36px] rounded-[3px] object-cover" />
                                     <div className="text-[#364A63] font-normal text-[14px] leading-[23.1px] text-left">Classy Modern Smart Watch</div>
@@ -243,17 +262,27 @@ function App() {
                                 <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto">1 </div>
                                 <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto text-right">$99.00</div>
                             </div>
+                            <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4 md:border-b border-[#DBDFEA] pb-4 pr-1 ">
+                                <div className="flex items-center gap-2">
+                                    <img src={Indigo} alt="Item" className="w-[36px] h-[36px] rounded-[3px] object-cover" />
+                                    <div className="text-[#364A63] font-normal text-[14px] leading-[23.1px] text-left">Classy Modern Smart Watch</div>
+                                </div>
+                                <div className="text-[#364A63] font-normal text-[14px] leading-[23.1px] my-auto">Cyan</div>
+                                <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto">L</div>
+                                <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto">1 </div>
+                                <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto text-right">$99.00</div>
+                            </div>
 
-                           {/* total count */}
-                            <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] gap-4">
+                            {/* total count */}
+                            <div className="grid grid-cols-[278px_1fr_1fr_1fr_1fr] pr-1 gap-4">
                                 <div className="text-[#373737] font-bold text-[16px] leading-[22px] -tracking-[0.2] text-left my-auto ">Total</div>
-                                <div className="text-[#8091A7] text-[14px] leading-[20px]"></div>
-                                <div className="text-[#8091A7] text-[14px] leading-[20px]"></div>
+                                <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto"></div>
+                                <div className="text-[#364A63] font-bold text-[14px] leading-[23.1px] my-auto"></div>
                                 <div className="text-[#8091A7] font-bold text-[14px] leading-[34px] -tracking-[0.2px]">1</div>
                                 <div className="text-[#8091A7] font-bold text-[18px] leading-[34px] -tracking-[0.2px] text-right">$79.00</div>
                             </div>
                         </div>
-                        <div className="flex justify-end gap-6 pt-[10px]">
+                        <div className="flex justify-end gap-6 pt-[10px] ">
                             <button
                                 onClick={handleCloseModal}
                                 className="px-[18px] py-2 rounded-[3px]  w-[152px] border border-1 border-[#DBDFEA] "
